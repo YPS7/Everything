c15002f13b7ad9ed8f7d6f64d00b2549d9b7f3f1
YPS7 <yashrok02@gmail.com>
2023-02-10 02:17:40 +0530

    update server

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codegram.onrender.com', {
+    const response = await fetch('https://everything.onrender.com', {
         method: 'POST',
 
 
